022ab8474c1eeeba3b4ae194c378dd9c2bde024c
Gordon Gao <gordonsdev@gmail.com>
2016-07-05 17:01:14 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,13 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+    "My name is Gordon"
+  end
+  get '/hometown' do
+    "My hometown is Qingdao"
+  end
+  get '/favorite-song' do
+    "My favorite song is what I don't know.."
+  end
 end
