6152f9846bf748aaf93b4e1db4b4dca8b142d1e5
blessedsoy <unmigong@gmail.com>
2016-07-12 08:42:32 -0400

    Sinatra Basic Routes Lab

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Unmi"
+    # erb :'medicines/index.html.erb'
+  end
+
+  get '/hometown' do
+    "My hometown is Seoul"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Candy"
+  end
+
 end
