9a0351a33585207ca6ef3d1a6ae15b4195be467d
eutopian <eutopia@gmail.com>
2016-08-05 10:45:08 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Margaret"
+    
+  end
+
+  get '/hometown' do
+    "My hometown is Xi'an"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is 'My Heart Will Go On'"
+  end
+
 end
