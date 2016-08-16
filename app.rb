1da4a2a2b51bde4f8c8ef5418ee256df837581b1
hafizrahman1 <hafiz.bd2009@gmail.com>
2016-08-16 17:22:55 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,22 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    "Hello, World!"
+  end
+
+  get '/name' do
+    "My name is Hafiz"
+  end
+
+  get '/hometown' do
+    "My hometown is kushtia, Bangladesh"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is quran recitation"
+  end
+
+
 end
