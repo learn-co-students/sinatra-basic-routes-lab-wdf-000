356463c4eb47b8e8921af57c3af0d08b468722cb
Flatiron School <github email address>
2016-08-17 15:35:55 -0400

    done

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    status = 200
+    "My name is Hanna"
+  end
+
+  get '/hometown' do
+    status = 404
+    "My hometown is Chicago"
+  end
+
+  get '/favorite-song' do
+    status = 200
+    "My favorite song is Chicago"
+  end
+  
 end
