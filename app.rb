99d7eb8f45153ad824b0fe4194c2e4b0d291f84e
Natalisp <nathalipat@gmail.com>
2016-08-19 10:48:07 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do 
+    "My name is Natalie"
+  end
+
+  get '/hometown' do 
+    "My hometown is Brest"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is La-la-la"
+  end
 end
