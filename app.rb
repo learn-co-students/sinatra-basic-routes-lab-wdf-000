48682bff202792182604f3296d141513417db915
Bibi Khadiza <limakhadiza@yahoo.com>
2016-08-28 15:49:39 -0400

    having so much fun with sinatra

--- app.rb
+++ app.rb
@@ -1,4 +1,23 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Bibi"
+  end
+
+  get '/hometown' do
+    "My hometown is Brooklyn"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is yet to be found"
+  end
+
+
+
+
+
+
+
 end
