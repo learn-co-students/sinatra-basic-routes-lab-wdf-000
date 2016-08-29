d8bd313d994788d5a21843ee5c540baf8c9c9f6a
sami4thewin <samiomarmohammed@gmail.com>
2016-08-29 12:05:56 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Sami."
+  end
+
+  get '/hometown' do
+    "My hometown is Manchester."
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Larry Bloom."
+  end
+
 end
