6f9d8536c3164c6d50fd8e3d9130067f34718aa1
Kelene Chung <simoy93@gmail.com>
2016-08-31 11:43:25 -0400

    Sinatra Basic Routes lab DONE

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+	get '/name' do 
+		"My name is K."
+	end
+
+	get '/hometown' do 
+		"My hometown is Harlem."
+	end
+
+	get '/favorite-song' do 
+		"My favorite song is Soca."
+	end
+
 end
